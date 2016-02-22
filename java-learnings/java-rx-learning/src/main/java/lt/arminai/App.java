cf984e8e07631549de5ce8e85abd98ac7b0fb835
Vytautas Arminas <vytautas.arminas@gmail.com>
2016-02-22 23:31:33 +0200

    Initial commit of javarx

---
+++ java-learnings/java-rx-learning/src/main/java/lt/arminai/App.java
@@ -0,0 +1,7 @@
+package lt.arminai;
+
+/**
+ * Created by Vytautas on 2016-02-22.
+ */
+public class App {
+}
---
+++ java-learnings/java-rx-learning/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Vytautas on 2016-02-22.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+
+    }
+}
