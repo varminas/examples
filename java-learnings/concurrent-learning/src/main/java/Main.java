92c39d36c7d069236d2dc364317a41a9b3889c51
Vytautas Arminas <vytautas.arminas@gmail.com>
2016-11-07 22:59:12 +0200

    Refactored concurrent pack

--- java-learnings/concurrent-learning/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by vytautas on 2016-11-07.
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
--- java-learnings/concurrent-learning/src/test/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by vytautas on 2016-11-07.
- */
-public class Test {
-}
