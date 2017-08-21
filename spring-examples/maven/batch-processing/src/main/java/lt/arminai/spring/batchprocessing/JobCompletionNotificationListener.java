a65b8aab53cf39059c8525ecd48ca35aa2d3c3ce
vytautas.arminas <vytautas.arminas@gmai.com>
2017-08-21 21:32:10 +0200

    Created full batch app

---
+++ spring-examples/maven/batch-processing/src/main/java/lt/arminai/spring/batchprocessing/JobCompletionNotificationListener.java
@@ -0,0 +1,36 @@
+package lt.arminai.spring.batchprocessing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+    private static final Logger logger = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            logger.info("JOB FINISHED. Time to verify the result");
+
+            List<Person> results = jdbcTemplate.query("SELECT first_name, last_name from people", (resultSet, i) ->
+                    new Person(resultSet.getString(1), resultSet.getString(2)));
+
+        results.forEach(result -> logger.info("Found <{}> in the database", result));
+        }
+    }
+}
